eb5575f43aeb02f05486e393afde212653e65a94
tucobenedicto <w99snet@gmail.com>
2023-10-09 20:57:35 +0200

    survey

---
+++ src/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+//import App from './pages/Home/index';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Survey from "./pages/Survey/survey"
+import Header from "./components/Header"
+import Error from "./components/Error"
+import Results from './pages/Results';
+import Freelances from './pages/Freelances';
+
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Router>
+        <Header />
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/survey/" element={<Survey />} />
+              <Route path="/survey/:questionNumber" element={<Survey />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/freelances" element={<Freelances />} />
+
+              <Route path="*" element={<Error />} />
+          </Routes>
+      </Router>
+  </React.StrictMode>,
+)
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
--- src/pages/Survey/survey.jsx
+++ src/pages/Survey/survey.jsx
@@ -1,9 +1,22 @@
+import { useParams , Link} from "react-router-dom";
+
 function Survey() {
-    return (
-        <div>
-            <h1>Questionnaire 🧮</h1>
-        </div>
-    )
+  const { questionNumber } = useParams();
+
+  
+   function sayHello(){
+    return parseInt(questionNumber+1)
+   }
+
+  return (
+    <div>
+      <h1>Questionnaire 🧮</h1>
+
+      <Link to={`/survey/${sayHello()}`}>Précédent</Link>
+
+      <h2>Question {questionNumber}</h2>
+    </div>
+  );
 }
 
-export default Survey
+export default Survey;
