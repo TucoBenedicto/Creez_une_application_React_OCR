f92d257af1046cbb8ad4793b4aff02c0b7e8479f
tucobenedicto <w99snet@gmail.com>
2023-10-10 22:03:37 +0200

    utilisation de  propTypes

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import DefaultPicture from "../../assets/profile.jpg";
+
+function Card({ label, title, picture }) {
+  return (
+    <div style={{ display: "flex", flexDirection: "column", padding: 15 }}>
+      <span>{label}</span>
+      <img src={picture} alt="freelance" height={80} width={80} />
+      <span>{title}</span>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  label: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string,
+};
+
+Card.defaultProps = {
+  label: "",
+  title: "",
+  picture: DefaultPicture,
+};
+
+export default Card;
